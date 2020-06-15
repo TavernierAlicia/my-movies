ba541fc50b4b256f734c6ec587d6d41f90448823
Alicia <alicia@ringover.com>
2020-06-15 18:22:30 +0200

    initial commit

---
+++ src/components/home/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import Header from './header';
+import Content from './content';
+
+class Home extends Component {
+  constructor() {
+    super();
+
+    // Initial State
+    this.state = {
+      title: 'React Movies',
+      mail: 'Enter your mail',
+      pwd: 'Enter your password',
+    };
+  }
+
+  render() {
+    const { title } = this.state;
+
+    return (
+      <div>
+        <Header title={title} />
+        <Content />
+      </div>
+    );
+  }
+}
+
+export default Home;
